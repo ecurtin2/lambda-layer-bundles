f1dfb7f94c755376322076b4a50f4fb4796fb083
Evan M Curtin <ecurtin2@illinois.edu>
2018-12-26 13:13:58 -0600

    update layer code to remove pipenv

--- layer_package.sh
+++ layer_package.sh
@@ -1,44 +1,10 @@
-#
-# layer-package.sh
-#
-# This requires pipenv installed and a version
-# of the python compatible with the layer runtime.
-#
-# Make sure to run this on an environment compatible
-# with aws lambda, I currently run it on an amazonlinux 
-# EC2 AMI.
-#
-# TODO: parameterize the installed dependencies and maybe 
-#       the layer name.
-#
-
-LAYER_NAME=datascience-python37
+LAYER_NAME=numpy-docker-py37
 ZIP_FILE=$LAYER_NAME.zip
-S3_BUCKET=<BUCKET_HERE>
-
-# Change if you know what you're doing.
-# Might want to just change python version and keep
-# the rest the same.
+S3_BUCKET=broadspire-lambda-layers
 PY_DIR='build/python/lib/python3.7/site-packages'
-
-echo 'Creating pipenv virtual environment...'
-pipenv --python 3.7
-pipenv shell
-
-###############################################################
-#
-# Change install command to be whatever
-#
-################################################################
-
-echo 'Installing dependencies into pipenv...'
-pipenv install sklearn pandas xgboost altair
-
-#################################################################
 
 echo 'Installing into local directory...'
 mkdir -p $PY_DIR
-pipenv lock -r > requirements.txt
 pip install -r requirements.txt --no-deps -t $PY_DIR
 
 cd build
@@ -55,19 +21,12 @@ find -type d -name doc -exec rm -rf {} \;
 find -type d -name docs -exec rm -rf {} \;
 
 echo 'Compile python files to .pyc'
-python3 -m compileall . -b -q
+python -m compileall . -b -q
 
-echo 'Removing .py  files'
 find . -name "*.py" -type f -delete
 
-echo 'zipping up requirements...'
 zip -r ../$ZIP_FILE .
 cd ..
 
-echo 'Uploading to s3...'
 aws s3 cp $ZIP_FILE s3://$S3_BUCKET
-
-echo 'Creating lambda layer from zip...'
-DESCRIPTION=`cat requirements.txt`
-
-aws lambda publish-layer-version --layer-name $LAYER_NAME	--compatible-runtimes python3.7 --zip-file fileb://$ZIP_FILE --description "$DESCRIPTION"
+aws lambda publish-layer-version --layer-name $LAYER_NAME --compatible-runtimes python3.7 --zip-file fileb://$ZIP_FILE --description "$DESCRIPTION"
